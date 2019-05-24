632ee01e63f04a1c55bfc2e018668d7195246c5c
Josh Jones <ohblonddev@gmail.com>
2019-05-24 21:42:06 +0100

    create square_sum function

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,24 @@
 
-fn is_unhappy(n:i32) -> bool {
-    let unhappy_markers: [i32; 8] = [89, 145, 42, 37, 58, 20, 4, 16];
+fn square_sum(n: u32) -> u32 {
+    let n_string: String = n.to_string();
+    let mut ss: u32 = 0;
+
+    for c in n_string.chars() {
+        match c.to_digit(10) {
+            Some(i) => ss += i,
+            _ => (),
+        }
+    }
+
+    return ss;
+}
+
+fn is_unhappy(n:u32) -> bool {
+    let unhappy_markers: [u32; 8] = [89, 145, 42, 37, 58, 20, 4, 16];
     return unhappy_markers.contains(&n)
 }
 
-fn is_happy(n: i32) -> bool {
+fn is_happy(n: u32) -> bool {
     if !is_unhappy(n) {
         return true
     } else {
