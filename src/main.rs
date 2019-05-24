007e6ac300031ef30155a149da815cef3849b522
Josh Jones <ohblonddev@gmail.com>
2019-05-24 22:43:34 +0100

    move is_first_it to binary

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
 extern crate happynum;
 
+fn is_first_it(n: u32) -> bool {
+    let n_string: String = n.to_string();
+    let digit_vec: Vec<u32> = n_string.chars()
+        .map(|s| s.to_digit(10).unwrap()).collect();
+
+    return true;
+}
+
 fn main() {
     let range = 100;
     let mut count = 0;
 
     for i in 1..range {
-        if happynum::is_happy(i) {
+        if is_first_it(i) && happynum::is_happy(i) {
             count += 1;
         }
     }
