cd4e0ed998ec21911122bea1e00e8d390735ad76
Josh Jones <ohblonddev@gmail.com>
2019-05-24 22:21:31 +0100

    split into binary and library

---
+++ src/lib.rs
@@ -0,0 +1,35 @@
+
+fn square_sum(n: u32) -> u32 {
+    let n_string: String = n.to_string();
+    let mut ss: u32 = 0;
+
+    for c in n_string.chars() {
+        match c.to_digit(10) {
+            Some(i) => {
+                ss += i.pow(2);
+            },
+            _ => (),
+        }
+    }
+
+    return ss;
+}
+
+fn is_unhappy(n:u32) -> bool {
+    let unhappy_markers: [u32; 8] = [89, 145, 42, 37, 58, 20, 4, 16];
+    return unhappy_markers.contains(&n)
+}
+
+pub fn is_happy(n: u32) -> bool {
+    let mut ss = square_sum(n);
+
+	loop {
+		if ss == 1 {
+			return true;
+		} else if is_unhappy(ss) {
+			return false;
+		} else {
+			ss = square_sum(ss);
+		}
+	}
+}
--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,11 @@
-
-fn square_sum(n: u32) -> u32 {
-    let n_string: String = n.to_string();
-    let mut ss: u32 = 0;
-
-    for c in n_string.chars() {
-        match c.to_digit(10) {
-            Some(i) => {
-                ss += i.pow(2);
-            },
-            _ => (),
-        }
-    }
-
-    return ss;
-}
-
-fn is_unhappy(n:u32) -> bool {
-    let unhappy_markers: [u32; 8] = [89, 145, 42, 37, 58, 20, 4, 16];
-    return unhappy_markers.contains(&n)
-}
-
-fn is_happy(n: u32) -> bool {
-    let mut ss = square_sum(n);
-
-	loop {
-		if ss == 1 {
-			return true;
-		} else if is_unhappy(ss) {
-			return false;
-		} else {
-			ss = square_sum(ss);
-		}
-	}
-}
+extern crate happynum;
 
 fn main() {
     let range = 100;
     let mut count = 0;
 
     for i in 1..range {
-        if is_happy(i) {
+        if happynum::is_happy(i) {
             count += 1;
         }
     }
