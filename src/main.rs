c5856952b24e151c8724d87bcdb1ee8fae9a8833
Josh Jones <ohblonddev@gmail.com>
2019-05-26 15:28:35 +0100

    abbreviate is_first_it

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,21 @@
 extern crate happynum;
 
 fn is_first_it(n: u32) -> bool {
-    let n_string: String = n.to_string();
-    let mut digit_vec: Vec<u32> = n_string.chars()
+    let mut digit_vec: Vec<u32> = n.to_string().chars()
         .map(|s| s.to_digit(10).unwrap()).collect();
     digit_vec.sort();
 
-    let sorted_n_string: String = digit_vec.iter().map(|d: &u32| -> String {
-        return d.to_string();
-    }).collect();
-    let sorted_n: u32 = sorted_n_string.parse::<u32>().unwrap();
+    let sorted_n: u32 = {
+        let sorted_n_string: String = digit_vec.iter()
+            .map(|d| d.to_string()).collect();
+        sorted_n_string.parse::<u32>().unwrap()
+    };
 
     return sorted_n == n;
 }
 
 fn main() {
-    let range = 10000000;
+    let range = 1000000;
     let mut count = 0;
 
     for i in 1..(range + 1) {
