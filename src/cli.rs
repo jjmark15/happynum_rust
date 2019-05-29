0774a67bf4726692482efa12bc834747618ac27b
Josh Jones <ohblonddev@gmail.com>
2019-05-29 22:28:07 +0100

    wip add cli

---
+++ src/cli.rs
@@ -0,0 +1,61 @@
+extern crate clap;
+use clap::{Arg, App};
+
+use std::time::{Duration, Instant};
+
+fn is_sorted(v: Vec<u32>) -> bool {
+    for (i, n) in v.iter().enumerate() {
+        match v.get(&i -1) {
+            Some(prev) => {
+                if prev > n {
+                    return false;
+                }
+            },
+            _ => (),
+        }
+    }
+    return true;
+}
+
+fn is_first_it(n: u32) -> bool {
+    let digit_vec: Vec<u32> = n.to_string().chars()
+        .map(|s| s.to_digit(10).unwrap()).collect();
+    return is_sorted(digit_vec);
+}
+
+fn run_happy_check(upper_bound: u32) {
+    extern crate happynum;
+
+    let mut count = 0;
+
+    let start = Instant::now();
+
+    for i in 1..(upper_bound + 1) {
+        if is_first_it(i) && happynum::is_happy(i) {
+            count += 1;
+        }
+    }
+
+    let duration: Duration = start.elapsed();
+
+    println!("{} distinct happy numbers found in {:?}", count, duration);
+}
+
+pub fn instantiate_cli() {
+    let matches = App::new("Distinct Happy Number Range Counter")
+                          .version("1.2.0")
+                          .author("Josh Jones <ohblonddev@gmail.com>")
+                          .about("Does awesome things")
+                          .arg(Arg::with_name("range")
+                               .short("r")
+                               .long("range")
+                               .value_name("RANGE")
+                               .help("Choose a range from 0 to number")
+                               .takes_value(true))
+                          .get_matches();
+
+    if let Some(range_end_str) = matches.value_of("range") {
+        let range_end: u32 = range_end_str.parse::<u32>().unwrap_or(0);
+        run_happy_check(range_end);
+    }
+}
