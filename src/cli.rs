cb814b0670b4ba9c21bf418e0c3f2529abdd0959
Josh Jones <ohblonddev@gmail.com>
2019-05-30 18:44:18 +0100

    build crate version into cli version

--- src/cli.rs
+++ src/cli.rs
@@ -37,17 +37,17 @@ fn run_happy_check(upper_bound: u32) {
 
 pub fn instantiate_cli() {
     let matches = App::new("Distinct Happy Number Range Counter")
-                          .version("1.2.0") // TODO: link to git tag version
-                          .author("Josh Jones <ohblonddev@gmail.com>")
-                          .about("Does awesome things")
-                          .arg(Arg::with_name("range")
-                               .short("r")
-                               .long("range")
-                               .value_name("RANGE")
-                               .help("Choose a range from 0 to number")
-                               .takes_value(true))
-                          .get_matches();
+                        .version(crate_version!())
+                        .author("Josh Jones <ohblonddev@gmail.com>")
+                        .about("Does awesome things")
+                        .arg(Arg::with_name("range")
+                            .short("r")
+                            .long("range")
+                            .value_name("RANGE")
+                            .help("Choose a range from 0 to number")
+                            .takes_value(true))
+                        .get_matches();
 
-    let range_end: u32 = value_t!(matches, "range", u32).unwrap_or(1000000);
+    let range_end: u32 = value_t!(matches, "range", u32).unwrap();
     run_happy_check(range_end);
 }
