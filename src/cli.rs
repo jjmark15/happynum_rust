835b913651357522ae47137c3399686df5ef9ce1
Josh Jones <ohblonddev@gmail.com>
2019-11-30 20:26:05 +0000

    Refactor with intellij (#18)
    * auto-format
    
    * write tests for lib

--- src/cli.rs
+++ src/cli.rs
@@ -1,14 +1,14 @@
-use clap::{Arg, App};
-use happynum::{distinct_is_happy_range};
 use std::process::exit;
+
+use clap::{App, Arg};
+
+use happynum::distinct_is_happy_range;
 
 fn run_happy_check(upper_bound: u32) {
     use std::time::{Duration, Instant};
 
     let start = Instant::now();
-
     let count = distinct_is_happy_range(upper_bound);
-
     let duration: Duration = start.elapsed();
     println!("{} distinct happy numbers found in {:?}", count, duration);
 }
@@ -19,16 +19,18 @@ fn f32_can_be_u32(n: f32) -> bool {
 
 pub fn instantiate_cli() {
     let matches = App::new("Distinct Happy Number Range Counter")
-                        .version(crate_version!())
-                        .author("Josh Jones <ohblonddev@gmail.com>")
-                        .about("Gets the count of distinct happy numbers in a range")
-                        .arg(Arg::with_name("range")
-                            .short("r")
-                            .long("range")
-                            .value_name("RANGE")
-                            .help("Choose a range from 0 to number")
-                            .takes_value(true))
-                        .get_matches();
+        .version(crate_version!())
+        .author("Josh Jones <ohblonddev@gmail.com>")
+        .about("Gets the count of distinct happy numbers in a range")
+        .arg(
+            Arg::with_name("range")
+                .short("r")
+                .long("range")
+                .value_name("RANGE")
+                .help("Choose a range from 0 to number")
+                .takes_value(true),
+        )
+        .get_matches();
 
     if let Some(_) = matches.value_of("range") {
         let range_end: f32 = value_t!(matches, "range", f32).unwrap();
