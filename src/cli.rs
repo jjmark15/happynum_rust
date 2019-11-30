8180fca2034a22ef2eebdf5b9b2b3adbc1408c80
Josh Jones <ohblonddev@gmail.com>
2019-11-30 18:48:21 +0000

    auto-format

--- src/cli.rs
+++ src/cli.rs
@@ -1,6 +1,8 @@
-use clap::{Arg, App};
-use happynum::{distinct_is_happy_range};
 use std::process::exit;
+
+use clap::{App, Arg};
+
+use happynum::distinct_is_happy_range;
 
 fn run_happy_check(upper_bound: u32) {
     use std::time::{Duration, Instant};
@@ -19,16 +21,16 @@ fn f32_can_be_u32(n: f32) -> bool {
 
 pub fn instantiate_cli() {
     let matches = App::new("Distinct Happy Number Range Counter")
-                        .version(crate_version!())
-                        .author("Josh Jones <ohblonddev@gmail.com>")
-                        .about("Gets the count of distinct happy numbers in a range")
-                        .arg(Arg::with_name("range")
-                            .short("r")
-                            .long("range")
-                            .value_name("RANGE")
-                            .help("Choose a range from 0 to number")
-                            .takes_value(true))
-                        .get_matches();
+        .version(crate_version!())
+        .author("Josh Jones <ohblonddev@gmail.com>")
+        .about("Gets the count of distinct happy numbers in a range")
+        .arg(Arg::with_name("range")
+            .short("r")
+            .long("range")
+            .value_name("RANGE")
+            .help("Choose a range from 0 to number")
+            .takes_value(true))
+        .get_matches();
 
     if let Some(_) = matches.value_of("range") {
         let range_end: f32 = value_t!(matches, "range", f32).unwrap();
--- src/lib.rs
+++ src/lib.rs
@@ -2,22 +2,22 @@
 
 /// Returns the sum of the square of all digits in `n`
 fn square_sum(n: u32) -> u32 {
-	let mut ss: u32 = 0;
-	let mut val: u32 = n;
+    let mut ss: u32 = 0;
+    let mut val: u32 = n;
 
-	while val > 0 {
-		ss += (val % 10).pow(2);
+    while val > 0 {
+        ss += (val % 10).pow(2);
 
-		// take advantage of losing decimal values in an ineger value
-		val /= 10;
-	}
-	return ss;
+        // take advantage of losing decimal values in an ineger value
+        val /= 10;
+    }
+    return ss;
 }
 
 /// Returns `false` if number is contained in list of unhappy numbers
-fn is_unhappy(n:u32) -> bool {
+fn is_unhappy(n: u32) -> bool {
     const UNHAPPY_MARKERS: [u32; 8] = [89, 145, 42, 37, 58, 20, 4, 16];
-    return UNHAPPY_MARKERS.contains(&n)
+    return UNHAPPY_MARKERS.contains(&n);
 }
 
 /// Checks if a number is a happy number.
@@ -31,15 +31,15 @@ fn is_unhappy(n:u32) -> bool {
 pub fn is_happy(n: u32) -> bool {
     let mut ss = square_sum(n);
 
-	loop {
-		if ss == 1 {
-			return true;
-		} else if is_unhappy(ss) {
-			return false;
-		} else {
-			ss = square_sum(ss);
-		}
-	}
+    loop {
+        if ss == 1 {
+            return true;
+        } else if is_unhappy(ss) {
+            return false;
+        } else {
+            ss = square_sum(ss);
+        }
+    }
 }
 
 /// Counts distinct happy numbers in a range.
@@ -50,13 +50,13 @@ pub fn is_happy(n: u32) -> bool {
 /// assert_eq!(711, happynum::distinct_is_happy_range(1000000));
 /// ```
 pub fn distinct_is_happy_range(n: u32) -> u32 {
-	let mut count = 0;
+    let mut count = 0;
     for i in 1..(n + 1) {
         if is_first_it(i) && is_happy(i) {
             count += 1;
         }
     }
-	return count;
+    return count;
 }
 
 /// Returns `true` if all digits in `n` are sorted
